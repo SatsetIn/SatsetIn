0569107a39e6d2cc57915adf452baeafb03620f0
sipatir08 <fathir080604@gmail.com>
2024-12-03 19:56:20 +0700

    push

--- config/db.go
+++ config/db.go
@@ -10,7 +10,7 @@ var MongoString string = os.Getenv("MONGOSTRING")
 
 var mongoinfo = atdb.DBInfo{
 	DBString: MongoString,
-	DBName:   "satsetin",
+	DBName:   "jajankuy",
 }
 
 var Mongoconn, ErrorMongoconn = atdb.MongoConnect(mongoinfo)
