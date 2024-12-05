947784506631e9ada676be36c4deec58b643ad7e
raniaayuni <raniaaayunii@gmail.com>
2024-12-05 19:53:28 +0700

    map.go

--- controller/map.go
+++ controller/map.go
@@ -12,43 +12,8 @@ import (
 	"go.mongodb.org/mongo-driver/bson"
 )
 
-// func GetRegion(respw http.ResponseWriter, req *http.Request) {
 
-// 	var longlat model.LongLat
-// 	json.NewDecoder(req.Body).Decode(&longlat)
-
-// 	filter := bson.M{
-// 		"border": bson.M{
-// 			"$geoIntersects": bson.M{
-// 				"$geometry": bson.M{ // Menggunakan $geometry, bukan $border
-// 					"type":        "Point",
-// 					"coordinates": []float64{longlat.Longitude, longlat.Latitude},
-// 				},
-// 			},
-// 		},
-// 	}
-	
-	
-// 	region, err := atdb.GetOneDoc[model.Region](config.MongoconnGeo, "region", filter)
-// 	if err != nil {
-// 		at.WriteJSON(respw, http.StatusNotFound, region)
-// 		return
-// 	}
-// 	at.WriteJSON(respw, http.StatusOK, region)
-// }
-
-//anton
 func GetRegion(respw http.ResponseWriter, req *http.Request) {
-	// Dekode token untuk autentikasi
-	// _, err := watoken.Decode(config.PublicKeyWhatsAuth, at.GetLoginFromHeader(req))
-	// if err != nil {
-	// 	var respn model.Response
-	// 	respn.Status = "Error : Token Tidak Valid"
-	// 	respn.Location = "Decode Token Error: " + at.GetLoginFromHeader(req)
-	// 	respn.Response = err.Error()
-	// 	at.WriteJSON(respw, http.StatusForbidden, respn)
-	// 	return
-	// }
 
 	// Parse koordinat dari body request
 	var longlat model.LongLat
@@ -104,12 +69,8 @@ func GetRegion(respw http.ResponseWriter, req *http.Request) {
 	at.WriteJSON(respw, http.StatusOK, geoJSON)
 }
 
-
-//GET ROADSSSSSSS
-// new anton add
+//GET ROADS
 func GetRoads(respw http.ResponseWriter, req *http.Request) {
-
-
 	var longlat model.LongLat
 	json.NewDecoder(req.Body).Decode(&longlat)	
 
