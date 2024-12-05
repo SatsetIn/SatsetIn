a3135d67ee320e12777f9047ff7120af2122be75
raniaayuni <raniaaayunii@gmail.com>
2024-12-05 17:35:58 +0700

    db.go

--- config/db.go
+++ config/db.go
@@ -14,3 +14,13 @@ var mongoinfo = atdb.DBInfo{
 }
 
 var Mongoconn, ErrorMongoconn = atdb.MongoConnect(mongoinfo)
+
+// Geospacial Database
+var MongoStringGeo string = "mongodb+srv://map:admin123@map.9ieis.mongodb.net/"
+
+var mongoinfoGeo = atdb.DBInfo{
+	DBString: MongoStringGeo,
+	DBName:   "maps",
+}
+
+var MongoconnGeo, ErrorMongoconnGeo = atdb.MongoConnect(mongoinfoGeo)
