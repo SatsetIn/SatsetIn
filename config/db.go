48aad456daf2d2c3a7011be1a73b8f3d6d7b3874
wafidadzka2105 <Adzkamukhayyar@gmail.com>
2024-12-12 14:02:22 +0700

    change connection

--- config/db.go
+++ config/db.go
@@ -6,13 +6,11 @@ import (
 	"github.com/gocroot/helper/atdb"
 )
 
-var MongoString string = os.Getenv("mongodb+srv://dewidesember20:ceZwVdzVHy4Qq3hk@cluster0.gkatt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-
+var MongoString string = os.Getenv("MONGOSTRING")
 var mongoinfo = atdb.DBInfo{
 	DBString: MongoString,
-	DBName:   "jajankuy",
+	DBName:   "florka",
 }
-
 var Mongoconn, ErrorMongoconn = atdb.MongoConnect(mongoinfo)
 
 // Geospacial Database
