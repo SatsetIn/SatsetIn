21f78d238e2c793c067b6b4a83870fdc69f1085a
wafidadzka2105 <Adzkamukhayyar@gmail.com>
2024-12-12 14:10:19 +0700

    Change DB

--- config/db.go
+++ config/db.go
@@ -9,12 +9,12 @@ import (
 var MongoString string = os.Getenv("MONGOSTRING")
 var mongoinfo = atdb.DBInfo{
 	DBString: MongoString,
-	DBName:   "florka",
+	DBName:   "jajankuy",
 }
 var Mongoconn, ErrorMongoconn = atdb.MongoConnect(mongoinfo)
 
 // Geospacial Database
-var MongoStringGeo string = "mongodb+srv://map:admin123@map.9ieis.mongodb.net/"
+var MongoStringGeo string = os.Getenv("MONGOSTRINGGEO")
 
 var mongoinfoGeo = atdb.DBInfo{
 	DBString: MongoStringGeo,
